1d3ffc77a161a7899d143c40d6b1c5c1f1bc97a3
liming.zeng <zlm20140709>
2020-12-19 23:28:49 +0800

    feat: add dubbo

---
+++ prophet-dubbo/src/main/java/cn/zlmisme/prophet/dubbo/ProphetDubboApplication.java
@@ -0,0 +1,11 @@
+package cn.zlmisme.prophet.dubbo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liming zeng
+ * @create 2020-11-23 11:30
+ */
+@SpringBootApplication
+public class ProphetDubboApplication {
+}
